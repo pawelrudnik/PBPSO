1adc3e634d24bd2662fb2d317603617db5e74404
Paweł <pawelrudnik92@gmail.com>
2017-01-17 01:14:38 +0100

    Przerobienie tablic na wektory

--- PORR/PORR.cpp
+++ PORR/PORR.cpp
@@ -7,15 +7,15 @@
 using namespace std;
 
 const int pop_size = 10;
-const int D = 4;
+const int D = 5;
 const int iterations = 10;
 const int R_min = -1;
 const int R_max = 1;
 const double omega = 0.8;
 const double c1 = 2.0;
 const double c2 = 2.0;
-const int profit[D] = { 4, 5, 3, 2 };
-const int weight[D] = { 1, 2, 3, 2 };
+const int profit[D] = { 4, 5, 3, 2, 5 };
+const int weight[D] = { 1, 2, 3, 2, 1 };
 const int M = 6;
 
 double velocity(double v_k, double x, double p_local, double p_global) {
@@ -32,31 +32,25 @@ bool binary(double x) {
 	return randomDouble(0.0, 1.0) <= L(x);
 }
 
-int fitness(bool x[])
+int fitness(vector<bool> x)
 {
 	int total_profit = 0;
 	int total_weight = 0;
-	for (int j = 0; j < D; j++) {
+	for (vector<bool>::size_type j = 0; j != x.size(); j++) {
 		total_profit += profit[j] * x[j];
 		total_weight += weight[j] * x[j];
 	}
 	return total_weight > M ? 0 : total_profit;
 }
 
-void CopyBoolArray(bool dest[], bool target[]) {
-	for (int i = 0; i < D; i++) {
-		target[i] = dest[i];
-	}
-}
-
 int main()
 {
-	double x[pop_size][D]; // pozycja
-	double v[pop_size][D]; // prêdkoœæ
-	bool px[pop_size][D]; // wektory binarne
-	bool best_px_local[pop_size][D]; // najlepsze wektory lokalne
-	bool best_px_global[D]; // najlepszy wektor globalny
-	int best_fpx_local[pop_size]; // najlepsze wyniki lokalne
+	vector<vector<double>> x(pop_size, vector<double>(D, 0)); // pozycja
+	vector<vector<double>> v(pop_size, vector<double>(D, 0)); // prêdkoœæ
+	vector<vector<bool>> px( pop_size, vector<bool>(D, 0) ); // wektory binarne
+	vector<vector<bool>> best_px_local(pop_size, vector<bool>(D, 0)); // najlepsze wektory lokalne
+	vector<bool> best_px_global(D); // najlepszy wektor globalny
+	vector<int> best_fpx_local(pop_size); // najlepsze wyniki lokalne
 	int best_fpx_global; // najlepszy wynik globalny
 
 	// Inicjalizacja wartoœci
@@ -87,11 +81,11 @@ int main()
 			fpx = fitness(px[i]);
 
 			if (fpx > best_fpx_local[i]) {
-				CopyBoolArray(px[i], best_px_local[i]);
+				best_px_local[i] = px[i];
 				best_fpx_local[i] = fpx;
 
 				if (fpx > best_fpx_global) {
-					CopyBoolArray(px[i], best_px_global);
+					best_px_global = px[i];
 					best_fpx_global = fpx;
 				}
 			}
